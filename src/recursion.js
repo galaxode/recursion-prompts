0157e62b726f032cf6fc20b5391c89185ff33a9d
galaxode <andrew.cookro@gmail.com>
2016-06-05 20:08:19 -0400

    Implemented range()

--- src/recursion.js
+++ src/recursion.js
@@ -73,6 +73,21 @@ var sumBelow = function(n) {
 // 6. Get the integers in range (x, y).
 // Example:  range(2, 9);  // [3, 4, 5, 6, 7, 8]
 var range = function(x, y) {
+	var highToLow = (x > y) ? true : false;
+  var length = Math.abs(y - x) - 1;
+
+	if (arguments[2] === undefined) {
+    arguments[2] = [];
+	}
+  if (length === 0 || length === -1) {
+    return arguments[2];
+	}
+	if (!highToLow) {
+    arguments[2].push(x+1);
+    return range(x+1, y, arguments[2]);
+  }
+  arguments[2].push(x-1);
+  return range(x-1, y, arguments[2]);
 };
 
 // 7. Compute the exponent of a number.
