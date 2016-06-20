10661b6c03e4d07825c7a7f0f82f8f9d4b954474
galaxode <andrew.cookro@gmail.com>
2016-06-20 18:41:17 -0400

    Finished recursion prompt #7.

--- src/recursion.js
+++ src/recursion.js
@@ -97,6 +97,19 @@ var range = function(x, y) {
 // Example:  exponent(4,3);  // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if (exp === 0) {
+  	return 1;
+  }
+  if (exp > 0 && exp % 2 === 0) {
+  	var y =  exponent(base, exp / 2);
+  	return y * y;
+  } else if (exp > 0) {
+  	return base * exponent(base, exp - 1);
+  }
+  if (exp < 0) {
+  	var y = exponent(base, -exp);
+  	return 1 / y;
+  }
 };
 
 // 8. Determine if a number is a power of two.
